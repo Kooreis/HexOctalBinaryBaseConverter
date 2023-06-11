f15f83d07b5b1a7bd915374333d36562ff5231da
Kooreis <kooreis.official@gmail.com>
2023-06-11 01:51:57 -0500

    In this update, the user is prompted to enter a number. The input is then stored in a string variable.

--- Hex_Octal_Binary_Base_Converter.cs
+++ Hex_Octal_Binary_Base_Converter.cs
@@ -1,4 +1,3 @@
-```CSharp
 using System;
 
 class Program
@@ -7,25 +6,5 @@ class Program
     {
         Console.WriteLine("Enter a number:");
         string input = Console.ReadLine();
-        Console.WriteLine("Enter the base of the number (2 for binary, 8 for octal, 10 for decimal, 16 for hexadecimal):");
-        int baseOfInput = int.Parse(Console.ReadLine());
-        Console.WriteLine("Enter the base to convert to (2 for binary, 8 for octal, 10 for decimal, 16 for hexadecimal):");
-        int baseToConvertTo = int.Parse(Console.ReadLine());
-
-        int decimalNumber = ConvertToDecimal(input, baseOfInput);
-        string convertedNumber = ConvertFromDecimal(decimalNumber, baseToConvertTo);
-
-        Console.WriteLine("The converted number is: " + convertedNumber);
-    }
-
-    static int ConvertToDecimal(string number, int baseOfNumber)
-    {
-        return Convert.ToInt32(number, baseOfNumber);
-    }
-
-    static string ConvertFromDecimal(int decimalNumber, int baseToConvertTo)
-    {
-        return Convert.ToString(decimalNumber, baseToConvertTo);
     }
 }
-```
