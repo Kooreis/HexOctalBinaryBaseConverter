c621adfae3c203cd5ee4c008848e8e958a7854b1
Kooreis <kooreis.official@gmail.com>
2023-06-12 01:51:57 -0500

    In this update, the user is prompted to enter the base of the number they previously entered. The input is then parsed to an integer and stored.

--- Hex_Octal_Binary_Base_Converter.cs
+++ Hex_Octal_Binary_Base_Converter.cs
@@ -6,5 +6,7 @@ class Program
     {
         Console.WriteLine("Enter a number:");
         string input = Console.ReadLine();
+        Console.WriteLine("Enter the base of the number (2 for binary, 8 for octal, 10 for decimal, 16 for hexadecimal):");
+        int baseOfInput = int.Parse(Console.ReadLine());
     }
 }
