ba44bd707c0c4a41970966cc200bb4a93f1f9870
Kooreis <kooreis.official@gmail.com>
2023-06-12 01:51:57 -0500

    This update adds the main function, which continuously prints a menu of conversion options for the user to choose from.

--- Hex_Octal_Binary_Base_Converter.py
+++ Hex_Octal_Binary_Base_Converter.py
@@ -1,10 +1,15 @@
-def convert_base(num, to_base=10, from_base=10):
-    if isinstance(num, str):
-        n = int(num, from_base)
-    else:
-        n = int(num)
-    alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    if n < to_base:
-        return alphabet[n]
-    else:
-        return convert_base(n // to_base, to_base) + alphabet[n % to_base]
+def main():
+    while True:
+        print("\n1. Hexadecimal to Decimal")
+        print("2. Hexadecimal to Octal")
+        print("3. Hexadecimal to Binary")
+        print("4. Decimal to Hexadecimal")
+        print("5. Decimal to Octal")
+        print("6. Decimal to Binary")
+        print("7. Octal to Decimal")
+        print("8. Octal to Hexadecimal")
+        print("9. Octal to Binary")
+        print("10. Binary to Decimal")
+        print("11. Binary to Hexadecimal")
+        print("12. Binary to Octal")
+        print("13. Exit")
